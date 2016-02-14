b3132704d45430f65ee14e12b577baf846c91975
A. Rosenberg Johansen <alexander@herhjemme.dk>
2016-02-14 11:26:12 +0100

    module ready

---
+++ frostings/__init__.py
@@ -0,0 +1,3 @@
+def joke():
+	return (u'Ween ist jaah\u00fcck git un Slotmeyer'
+					u'beiberhund simmer dick')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Frostings',
+	version='0.1',
+	description='data loader in python',
+	url='https://github.com/alrojo/Frostings',
+	author='Frostings contributers',
+	author_email='frostings@example.com',
+	license='',
+	packages=['frostings'],
+	zip_safe=False)
