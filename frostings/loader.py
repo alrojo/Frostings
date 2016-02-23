bc64989da277ddf1a6a1e92b099a96bd7e8c5927
Alexander Rosenberg Johansen <alexander@herhjemme.dk>
2016-02-23 17:15:50 +0100

    Renaming to sample + many more things

--- frostings/loader.py
+++ frostings/loader.py
@@ -18,81 +18,98 @@ class LoadMethod(object):
 	def __call__(self, idx):
 		return self.samples[idx]
 
-class ElemGenerator(object):
+class SampleInfo(object):
+	
+	def __init__(self, sample_length, sample_idx=None):
+		self.samples_length = samples_length
+		self.samples_idx = samples_idx
+		print("SampleInfo initated")
 
-	def __init__(self, LoadMethod, shuffle=False, repeat=False):
-		self.LoadMethod = LoadMethod
-		self.samples_length = len(self.LoadMethod.samples)
-		self.samples_idx = range(self.samples_length)
+class SampleGenerator(object):
+
+	def __init__(self, load_method, sample_info, shuffle=False, repeat=False):
+		self.load_method = load_method
+		self.sample_info = sample_info
 		self.shuffle = shuffle
 		self.repeat = repeat
 		print("ElemGenerator initiated")
 
 	def _shuffle_paths(self, state):
-		print("_shuffle_paths started")
 		np.random.set_state(state)
 		np.random.shuffle(self.samples_idx)
-		print("paths have been shuffled")
 
-	def gen_elem(self):
-		print("gen_elem started")
+	def gen_sample(self):
 		while True:
 			if self.shuffle:
 				state = np.random.get_state()
 				self._shuffle_paths(state)
-			print("sh*ts going down")
-			for num in xrange(self.samples_length):
-				yield self.LoadMethod(num)
+			for num in xrange(self.samples_info.samples_length):
+				yield self.load_method(num)
 			if not self.repeat:
 				break
 
-class ElemInfo(object):
+class BatchInfo(object):
 
-	def __init__(self, labels=False, elem_shape=None, label_shape=None, elem_dtype=None, label_dtype=None):
-		self.labels = labels
-		self.elem_shape = elem_shape
-		self.label_shape = label_shape
-		self.elem_dtype = elem_dtype
-		self.label_dtype = label_dtype
+	def __init__(self, batch_size):
+		self.batch_size = batch_size
+		print("BatchInfo initiated")
+
+class BatchGenerator(object):
+
+	def __init__(self, sample_generator, batch_info):
+		self.samples_generator = sample_generator
+		self.batch_info = batch_info
+
+	def _make_batch_holder(self):
+		pass # undecided on the general purpose structure ... See examples for implementation
+
+	def _make_batch(self):
+		pass # undecided on the general purpose structure ... See examples for implementation
+
+	def gen_batch(self):
+		self.sample_idx = 0
+		for sample in self.sample_generator.gen_sample()
+			self.samples.append(sample)
+			self.sample_idx += 1
+			if self.sample_idx >= self.batch_info.batch_size:
+				yield self._make_batch()
+		if self.sample_idx > 0:
+			yield self._make_batch()
 
 class ChunkInfo(object):
 
 	def __init__(self, chunk_size=4096, num_chunks=800):
 		self.chunk_size = chunk_size
 		self.num_chunks = num_chunks
+		print("ChunkInfo initiated")
 
 class ChunkGenerator(object):
 
-	def __init__(self, ElemGenerator, ElemInfo=None, ChunkInfo=None, rng=np.random):
-		self.ElemGenerator = ElemGenerator
-		self.ElemInfo = ElemInfo
-		self.ChunkInfo = ChunkInfo
+	def __init__(self, batch_generator, chunk_info, rng=np.random):
+		self.batch_generator = batch_generator
+		self.chunk_info = chunk_info
 		self.rng = rng
+		self.batches = []
 		print("ChunkGenerator initiated")
 
-	def _make_chunk(self):
-		self.chunk = []
-		chunk_elem = np.empty(self.ChunkInfo.chunk_size, dtype='object')
-		self.chunk.append(chunk_elem)
-		if self.ElemInfo is not None:
-			if self.ElemInfo.labels:
-				chunk_label = np.empty(self.ChunkInfo.chunk_size, dtype='object')
-				self.chunk.append(chunk_label)
+	def _make_chunk_holder(self):
+		self.chunk = [] # not in __init__ to reset it at every call
+		self.chunk.append([]) # where the batch is located
+		self.chunk.append(self.batch_idx) # pasting the idx
+		self.batch_idx = 0
 
-	def _add_chunk(self, sample, idx):
-		for s, c in zip(sample, self.chunk):
-			c[idx] = s
+	def _make_chunk(self):
+		self._make_chunk_holder()
+		for _ in range(len(self.batches)): # smarter solution?
+			self.chunk[0].append(self.batches.pop())
+		return self.chunk
 
 	def gen_chunk(self):
-		print("get_chunk started")
-		self._make_chunk()
-		idx = 0
-		for sample in self.ElemGenerator.gen_elem():
-			self._add_chunk(sample, idx)
-			idx += 1
-			if idx >= self.ChunkInfo.chunk_size:
-				yield self.chunk, idx
-				self._make_chunk() # Cleaning the chunk
-				idx = 0
-		if idx > 0:
-			yield self.chunk, idx
+		self.batch_idx = 0
+		for batch in self.batch_generator.gen_batch():
+			self.batches.append(batch)
+			self.batch_idx += 1
+			if self.batch_idx >= self.chunk_info.chunk_size:
+				yield self._make_chunk()
+		if self.batch_idx > 0:
+			yield self._make_chunk()
